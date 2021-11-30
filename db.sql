595ddcf6d37463c5ec86c0fb4531782113778c62
kauederp <36017642+kauederp@users.noreply.github.com>
2021-11-29 23:49:57 -0300

    Delete db.sql

--- db.sql
+++
@@ -1,8 +0,0 @@
-create database chat;
-use chat;
-create table t_message(
-    id_message int primary key auto_increment not null,
-    nick varchar(20) not null,
-    txt_message text not null, 
-    tstamp int not null
-);
